eb48264f27eec19c3ebfb5bb3c31d78fd3c743a1
sanks-the-coder <sj22316079@utg.edu.gm>
2025-09-09 19:10:07 +0000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {SignIn} from './components/authentication/sign-in/sign-in';
 import {SignUp} from './components/authentication/sign-up/sign-up';
+import {Dashboard} from './components/admin/dashboard/dashboard';
 
 export const routes: Routes = [
   {path: '', component: SignIn},
   {path: 'sign-in', component: SignIn},
-  {path: 'sign-up', component: SignUp}
+  {path: 'sign-up', component: SignUp},
+  {path: 'dashboard', component: Dashboard}
 ];
